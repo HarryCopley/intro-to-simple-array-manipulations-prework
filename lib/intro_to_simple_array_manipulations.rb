c1f53a0d2be1fb147bcc8739db6f253d81d7f5f0
generous-program-0200 <harrycopley@vultuslimited.com>
2018-12-08 02:11:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,61 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(0..-3)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift[0..2]
+end
+
+def using_concat(array1, array2)
+  array1.concat array2
+end
+
+def using_insert(array, element)
+  array.insert(4,(element))
+end
+
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+
+def using_delete(array, string)
+if array.include?(string)
+end
+end
+
+def using_delete_at(array, integer)
+  x = integer.to_i
+array.delete(x)
+end
+
+
+
+
+
+
+
+
+
+
